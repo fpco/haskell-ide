7158366d8221f99dd6d2269dbec802ea19402466
Michael Snoyman <michael@snoyman.com>
2012-11-06 10:53:54 +0200

    Code cleanup

--- project-template/Text/ProjectTemplate.hs
+++ project-template/Text/ProjectTemplate.hs
@@ -5,20 +5,11 @@
 module Text.ProjectTemplate where
 
 import           ClassyPrelude.Conduit
-import           Control.Monad                (unless)
-import           Control.Monad.Trans.Resource (runExceptionT)
 import           Control.Monad.Writer         (MonadWriter, tell)
-import qualified Data.ByteString              as S
 import qualified Data.ByteString.Base64       as B64
-import           Data.Conduit.Binary          (sinkFile)
-import           Data.Conduit.List            (sinkNull)
-import           Data.Conduit.List            (consume)
-import qualified Data.Conduit.Text            as CT
 import           Data.Functor.Identity        (runIdentity)
-import           Data.Text.Encoding           (encodeUtf8)
 import           Filesystem                   (createTree)
-import           Filesystem.Path.CurrentOS    (directory, encode, encodeString,
-                                               fromText)
+import           Filesystem.Path.CurrentOS    (directory, encode, fromText)
 
 fsPerFile :: MonadResource m
           => FilePath -- ^ root
@@ -26,7 +17,7 @@ fsPerFile :: MonadResource m
           -> Sink ByteString m ()
 fsPerFile root rel = do
     liftIO $ createTree $ directory fp
-    sinkFile $ encodeString fp
+    writeFile fp
   where
     fp = root </> rel
 
@@ -38,12 +29,12 @@ memPerFile fp = do
     lift $ tell $ singleton fp $ fromChunks bss
 
 unpackMultiFile
-    :: MonadResource m
+    :: MonadThrow m
     => (FilePath -> Sink ByteString m ()) -- ^ receive individual files
     -> (Text -> Text) -- ^ fix each input line, good for variables
     -> Sink ByteString m ()
 unpackMultiFile perFile fixLine =
-    CT.decode CT.utf8 =$ CT.lines =$ map fixLine =$ start
+    decodeUtf8 =$ lines =$ map fixLine =$ start
   where
     start =
         await >>= maybe (return ()) go
@@ -54,7 +45,7 @@ unpackMultiFile perFile fixLine =
                 Just (fp', isBinary) -> do
                     let src
                             | isBinary  = binaryLoop
-                            | otherwise = textLoop
+                            | otherwise = textLoop True
                     src =$ perFile (fromText fp')
                     start
 
@@ -62,16 +53,16 @@ unpackMultiFile perFile fixLine =
         await >>= maybe (error "binaryLoop needs 1 line") go
       where
         go = yield . B64.decodeLenient . encodeUtf8
-    textLoop =
+    textLoop isFirst =
         await >>= maybe (return ()) go
       where
         go t =
             case getFileName t of
                 Just{} -> leftover t
                 Nothing -> do
+                    unless isFirst $ yield "\n"
                     yield $ encodeUtf8 t
-                    yield "\n"
-                    textLoop
+                    textLoop False
 
     getFileName t =
         case words t of
@@ -79,27 +70,30 @@ unpackMultiFile perFile fixLine =
             ["{-#", "START_FILE", "BASE64", fn, "#-}"] -> Just (fn, True)
             _ -> Nothing
 
-createMultiFile
-    :: MonadIO m
-    => FilePath -- ^ folder containing the files
-    -> Conduit FilePath m ByteString -- ^ FilePath is relative to containing folder
-createMultiFile root = do
-    awaitForever handleFile
+loadFromFS :: MonadIO m
+           => FilePath -- ^ root folder
+           -> FilePath -- ^ relative path
+           -> (FilePath, m ByteString)
+loadFromFS root rel =
+    (fp, readFile fp)
   where
-    handleFile fp' = do
-        bs <- readFile fp
-        case runIdentity $ runExceptionT $ yield bs $$ CT.decode CT.utf8 =$ sinkNull of
-            Left{} -> do
-                yield "{-# START_FILE BASE64 "
-                yield $ encode fp'
-                yield " #-}\n"
-                yield $ B64.encode bs
-                yield "\n"
-            Right{} -> do
-                yield "{-# START_FILE "
-                yield $ encode fp'
-                yield " #-}\n"
-                yield bs
-                unless ("\n" `S.isSuffixOf` bs) $ yield "\n"
-      where
-        fp = root </> fp'
+    fp = root </> rel
+
+createMultiFile
+    :: Monad m
+    => Conduit (FilePath, m ByteString) m ByteString
+createMultiFile = awaitForever $ \(fp, getBS) -> do
+    bs <- lift getBS
+    case runIdentity $ runExceptionT $ yield bs $$ decodeUtf8 =$ sinkNull of
+        Left{} -> do
+            yield "{-# START_FILE BASE64 "
+            yield $ encode fp
+            yield " #-}\n"
+            yield $ B64.encode bs
+            yield "\n"
+        Right{} -> do
+            yield "{-# START_FILE "
+            yield $ encode fp
+            yield " #-}\n"
+            yield bs
+            yield "\n"
